448808d9d42de0739673d71f01f30a4d80601dbe
Johnatan Ortiz <6403@holbertonstudents.com>
2023-09-19 13:31:39 -0500

    fixing

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,10 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (err, response, body) => {
-  json_file = JSON.parse(body)
+  if (err) {
+    console.log(err);
+  }
+  const json_file = JSON.parse(body);
   let len = 0;
   for ( const movie of json_file.results ) {
     for ( const character of movie.characters ) {
