e572f739f0065774c2081fffe36f51cc27f75c99
Johnatan Ortiz <6403@holbertonstudents.com>
2023-09-19 13:00:13 -0500

    error handle

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,6 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${id}`;
 
-
 request.get(url, (err, response, body) => {
-  console.log(JSON.parse(body).title);
+  console.log(JSON.parse(body).title || err);
 });
