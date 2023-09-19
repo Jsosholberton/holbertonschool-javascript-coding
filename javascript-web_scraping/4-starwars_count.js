4cd9a5c7bd67b8851a965ef96de166215b160fbd
Johnatan Ortiz <6403@holbertonstudents.com>
2023-09-19 13:29:32 -0500

    fix?

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-character_id = 18;
 
 request.get(url, (err, response, body) => {
   json_file = JSON.parse(body)
   let len = 0;
   for ( const movie of json_file.results ) {
-    for ( const character in movie.characters ) {
-      if ( character == character_id ) {
-        len += 1;
+    for ( const character of movie.characters ) {
+      if ( character.includes(18) ) {
+        len++;
       }
     }
   }
